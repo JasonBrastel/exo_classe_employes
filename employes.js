c0983abe0ba281af2aaca63cbd6ed1d5013ee11a
Jason <jasonbrastel30@gmail.com>
2023-11-07 16:59:32 +0100

    tableau_en_cours

--- employes.js
+++ employes.js
@@ -1,33 +1,57 @@
-let today = new Date();
+let today = new Date()
 
 
-
-class Employe {
-    constructor(nom,prenom,datedEmbauche,poste,salaireKBrutAnnuel,service){
+class Employes {
+    constructor(nom, prenom, dateEmbauche, poste, salaireKBrutAnnuel, service) {
         this.nom = nom;
         this.prenom = prenom;
-        this.datedEmbauche=datedEmbauche;
-        this.poste=poste;
-        this.salaireKBrutAnnuel=salaireKBrutAnnuel;
-        this.service=service;
+        this.dateEmbauche = new Date(dateEmbauche);
+        this.poste = poste;
+        this.salaireKBrutAnnuel = salaireKBrutAnnuel;
+        this.service = service;
     }
 
-Anciennete(today){
-    
-    today -= this.datedEmbauche ;
-    console.log("L'employé bosse ici depuis",+today+ " ans");
-}
+    getAnciennete() {
 
-Prime(){
+        
+        let diffAnnee = today.getFullYear() - this.dateEmbauche.getFullYear();
 
-   let primeAnnuel=(this.salaireKBrutAnnuel * 5) /100;
-   let primeAnciennete =(this.salaireKBrutAnnuel * 2) /100;
+        return diffAnnee;
 
 
-
-   
-}
+    }
 
 
 
-}
+    primeAnnuel() {
+
+        if (today.getMonth() == 10 && today.getDate() == 7) {
+            let primeAnnuel = (this.salaireKBrutAnnuel * 5) / 100; // primeAnnuel 750euro
+
+
+            let primeAnciennete = (this.salaireKBrutAnnuel * 2) / 100; //prime de 300 euro
+            
+                if (this.getAnciennete() >= 0) {
+                    primeAnciennete = primeAnciennete * this.getAnciennete()
+                }
+           
+            let totalPrime = 0;
+            totalPrime = primeAnnuel + primeAnciennete;
+
+            
+            return console.log ("L'employé "+this.nom+ " aura " +totalPrime+" de prime cette année ");
+        
+        }
+        
+        else { 
+            return console.log("La prime sera disponible le 30 novembre de chaque année") }
+    }
+
+    nombreEmploye(){
+        nombreEmploye = table_employe.length;
+        console.log(nombreEmploye);
+        return(nombreEmploye);
+     }
+
+}//fermeture classe
+
