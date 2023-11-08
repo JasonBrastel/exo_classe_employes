9fe75c3acdbdc84616fb30ef2245e8cd0c541fce
Jason <jasonbrastel30@gmail.com>
2023-11-08 16:47:25 +0100

    exercice_8

--- employes.js
+++ employes.js
@@ -2,56 +2,59 @@ let today = new Date()
 
 
 class Employes {
-    constructor(nom, prenom, dateEmbauche, poste, salaireKBrutAnnuel, service) {
+    constructor(nom, prenom, dateEmbauche, poste, salaireKBrutAnnuel, service, agence) {
         this.nom = nom;
         this.prenom = prenom;
         this.dateEmbauche = new Date(dateEmbauche);
         this.poste = poste;
         this.salaireKBrutAnnuel = salaireKBrutAnnuel;
         this.service = service;
+        this.agence = agence;
+        if(this.getAnciennete>1){
+            this.chequeVacance = true;
+        }
+        else{
+            this.chequeVacance = false;
+        }
     }
 
     getAnciennete() {
 
-        
+
         let diffAnnee = today.getFullYear() - this.dateEmbauche.getFullYear();
-
         return diffAnnee;
-
-
     }
-
-
-
     primeAnnuel() {
 
-        if (today.getMonth() == 10 && today.getDate() == 7) {
+        if (today.getMonth() == 10 && today.getDate() == 8) {
             let primeAnnuel = (this.salaireKBrutAnnuel * 5) / 100; // primeAnnuel 750euro
 
-
             let primeAnciennete = (this.salaireKBrutAnnuel * 2) / 100; //prime de 300 euro
-            
-                if (this.getAnciennete() >= 0) {
-                    primeAnciennete = primeAnciennete * this.getAnciennete()
-                }
-           
+
+            if (this.getAnciennete() >= 0) {
+                primeAnciennete = primeAnciennete * this.getAnciennete()
+            }
+
             let totalPrime = 0;
             totalPrime = primeAnnuel + primeAnciennete;
 
-            
-            return console.log ("L'employé "+this.nom+ " aura " +totalPrime+" de prime cette année ");
-        
+            let totalSalaire = 0
+            totalSalaire = this.salaireKBrutAnnuel + totalPrime;
+
+            return totalSalaire;
+
         }
-        
-        else { 
-            return console.log("La prime sera disponible le 30 novembre de chaque année") }
+
     }
+}
 
-    nombreEmploye(){
-        nombreEmploye = table_employe.length;
-        console.log(nombreEmploye);
-        return(nombreEmploye);
-     }
+class Agences {
 
-}//fermeture classe
-
+    constructor(nom, adresse, code_postal, ville, mode_de_restauration) {
+        this.nom = nom;
+        this.adresse = adresse;
+        this.code_postal = code_postal;
+        this.ville = ville;
+        this.mode_de_restauration = mode_de_restauration
+    }
+}
